82d6937f3fb8e0f4bd74b2539dfaf506fd1d0936
Aswederry <aswederry@gmail.com>
2023-04-20 21:53:15 +0200

    Nuovo Progetto
    Aggiunto nuovo progetto, con immagini, e un piccolo test.

---
+++ PrendiTuttoVb/PrendiTuttoVb/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        test = New PictureBox()
+        CType(test, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' test
+        ' 
+        test.BackColor = Color.Transparent
+        test.BackgroundImageLayout = ImageLayout.Stretch
+        test.Location = New Point(264, 267)
+        test.Name = "test"
+        test.Size = New Size(77, 117)
+        test.TabIndex = 0
+        test.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(test)
+        Name = "Form1"
+        Text = "Form1"
+        CType(test, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents test As PictureBox
+End Class
---
+++ PrendiTuttoVb/PrendiTuttoVb/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Dim Images As Image(,) = New Image(,) {{My.Resources.Asso1}, {My.Resources.Asso2}, {My.Resources.Asso3}, {My.Resources.Asso4}}
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim a As New Random
+        Dim b As Integer
+
+        b = a.Next(0, 4)
+
+        test.BackgroundImage = Images(b, 0)
+    End Sub
+End Class
